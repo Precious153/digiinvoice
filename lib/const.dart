f21243a92e8023022602d2723eb0301fa5af91f5
Devsinglad <simonsinglad5@gmail.com.com>
2022-11-09 18:08:40 +0100

    init commit

---
+++ lib/const.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const image1 = 'assets/images/Frame 17.png';
+const image2 = 'assets/images/4954326 1.png';
+const image3 = 'assets/images/4170013 1.png';
+const image4 = 'assets/images/4023504 1.png';
+const image5 = 'assets/images/3969450 1.png';
+const image6 = 'assets/images/3071357 1.png';
+
+const mainColor = Colors.blueAccent;
+const textColor = Color(0xff797979);
